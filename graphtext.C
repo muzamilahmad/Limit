ea4bb67f3233a9282559b3d69059c55e8514e344
Muzamil Ahmad Bhat <muzamilphy786@gmail.com>
2017-10-01 15:26:40 +0530

    Create graphtext.C

---
+++ graphtext.C
@@ -0,0 +1,39 @@
+
+void graphtext() {
+   TCanvas *c = new TCanvas("c","best of luck",700,500);
+   c->SetGrid();
+
+   const Int_t n = 10;
+   TGraph *gr = new TGraph(n);
+   gr->SetTitle("Muzamil");
+   gr->SetMarkerStyle(20);
+   TExec *ex = new TExec("ex","drawtext();");
+   gr->GetListOfFunctions()->Add(ex);
+
+   Double_t x, y;
+   for (Int_t i=0;i<n;i++) {
+      x = i*0.1;
+      y = 10*sin(x+0.2);
+      gr->SetPoint(i,x,y);
+
+   }
+   gr->Draw("ALP");
+}
+
+void drawtext()
+{
+   Int_t i,n;
+   Double_t x,y;
+   TLatex *l;
+
+   TGraph *g = (TGraph*)gPad->GetListOfPrimitives()->FindObject("Graph");
+   n = g->GetN();
+   for (i=1; i<n; i++) {
+      g->GetPoint(i,x,y);
+      l = new TLatex(x,y+0.2,Form("%4.2f",y));
+      l->SetTextSize(0.025);
+      l->SetTextFont(42);
+      l->SetTextAlign(21);
+      l->Paint();
+}
+}
